aca12fb413c384dee4829accd34128629a1ff90a
Corentin Leffy <corentin.leffy@gmail.com>
2021-03-13 18:52:59 +0100

    I'll explain this when I'm sober .. or revert it

--- kotlin/src/main/kotlin/TennisGame1.kt
+++ kotlin/src/main/kotlin/TennisGame1.kt
@@ -22,13 +22,13 @@ class TennisGame1(player1Name: String, player2Name: String) : TennisGame {
     private fun aPlayerHasAdvantage() = player1.points >= 4 || player2.points >= 4
 
     private fun endGameScores(): String {
-        if (aPlayerIsLeadingByOnePoint()) return "Advantage ${leadingPlayer().name}"
-        return "Win for ${leadingPlayer().name}"
+        if (aPlayerIsLeadingByOnePoint()) return "Advantage ${leadingPlayer.name}"
+        return "Win for ${leadingPlayer.name}"
     }
 
     private fun aPlayerIsLeadingByOnePoint() = abs(player1.points - player2.points) == 1
 
-    private fun leadingPlayer() = player1.takeIf { player1.points > player2.points } ?: player2
+    private val leadingPlayer get() = player1.takeIf { player1.points > player2.points } ?: player2
 
     private fun scoresAreEqual() = player1.points == player2.points
 
