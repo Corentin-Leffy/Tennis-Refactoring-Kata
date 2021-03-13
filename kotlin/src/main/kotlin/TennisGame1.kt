059c3dac12265710db66fca95076add7116d835d
Corentin Leffy <corentin.leffy@gmail.com>
2021-03-13 18:14:08 +0100

    Don't push this commit

--- kotlin/src/main/kotlin/TennisGame1.kt
+++ kotlin/src/main/kotlin/TennisGame1.kt
@@ -1,30 +1,30 @@
 class TennisGame1(private val player1Name: String, private val player2Name: String) : TennisGame {
 
-    private var score1: Int = 0
-    private var score2: Int = 0
+    private var pointsPlayer1: Int = 0
+    private var pointsPlayer2: Int = 0
 
     override fun wonPoint(playerName: String) {
         if (playerName === "player1")
-            score1 += 1
+            pointsPlayer1 += 1
         else
-            score2 += 1
+            pointsPlayer2 += 1
     }
 
     override fun getScore(): String {
         if (scoresAreEqual()) {
             return scoreEquality()
         }
-        if (score1 >= 4 || score2 >= 4) {
+        if (pointsPlayer1 >= 4 || pointsPlayer2 >= 4) {
             return endGameScores()
         }
         var score = ""
         var tempScore: Int
         for (i in 1..2) {
             if (i == 1)
-                tempScore = score1
+                tempScore = pointsPlayer1
             else {
                 score += "-"
-                tempScore = score2
+                tempScore = pointsPlayer2
             }
             score += scoreOf(tempScore)
         }
@@ -40,7 +40,7 @@ class TennisGame1(private val player1Name: String, private val player2Name: Stri
     }
 
     private fun endGameScores(): String {
-        val minusResult = score1 - score2
+        val minusResult = pointsPlayer1 - pointsPlayer2
         return when {
             minusResult == 1 -> "Advantage player1"
             minusResult == -1 -> "Advantage player2"
@@ -49,10 +49,10 @@ class TennisGame1(private val player1Name: String, private val player2Name: Stri
         }
     }
 
-    private fun scoresAreEqual() = score1 == score2
+    private fun scoresAreEqual() = pointsPlayer1 == pointsPlayer2
 
     private fun scoreEquality() =
-        when (score1) {
+        when (pointsPlayer1) {
             0 -> "Love-All"
             1 -> "Fifteen-All"
             2 -> "Thirty-All"
