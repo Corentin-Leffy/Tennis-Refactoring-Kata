b91935905d96fd1ee5ea2823d3571d6517196a7f
Corentin Leffy <corentin.leffy@gmail.com>
2021-03-13 18:02:39 +0100

    unionfind is no longer being molested.

--- kotlin/src/main/kotlin/TennisGame1.kt
+++ kotlin/src/main/kotlin/TennisGame1.kt
@@ -1,11 +1,11 @@
 class TennisGame1(private val player1Name: String, private val player2Name: String) : TennisGame {
 
-    private var m_score1: Int = 0
+    private var score1: Int = 0
     private var m_score2: Int = 0
 
     override fun wonPoint(playerName: String) {
         if (playerName === "player1")
-            m_score1 += 1
+            score1 += 1
         else
             m_score2 += 1
     }
@@ -13,15 +13,15 @@ class TennisGame1(private val player1Name: String, private val player2Name: Stri
     override fun getScore(): String {
         var score = ""
         var tempScore: Int
-        if (m_score1 == m_score2) {
-            score = when (m_score1) {
+        if (score1 == m_score2) {
+            score = when (score1) {
                 0 -> "Love-All"
                 1 -> "Fifteen-All"
                 2 -> "Thirty-All"
                 else -> "Deuce"
             }
-        } else if (m_score1 >= 4 || m_score2 >= 4) {
-            val minusResult = m_score1 - m_score2
+        } else if (score1 >= 4 || m_score2 >= 4) {
+            val minusResult = score1 - m_score2
             score = when {
                 minusResult == 1 -> "Advantage player1"
                 minusResult == -1 -> "Advantage player2"
@@ -31,7 +31,7 @@ class TennisGame1(private val player1Name: String, private val player2Name: Stri
         } else {
             for (i in 1..2) {
                 if (i == 1)
-                    tempScore = m_score1
+                    tempScore = score1
                 else {
                     score += "-"
                     tempScore = m_score2
