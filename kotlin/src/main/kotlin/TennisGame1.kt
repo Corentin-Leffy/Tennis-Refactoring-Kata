70c66a20afb6e1257f45c64833e9516f24cf8af0
Corentin Leffy <corentin.leffy@gmail.com>
2021-03-13 18:08:34 +0100

    Your commit is writing checks your merge can't cash.

--- kotlin/src/main/kotlin/TennisGame1.kt
+++ kotlin/src/main/kotlin/TennisGame1.kt
@@ -33,7 +33,7 @@ class TennisGame1(private val player1Name: String, private val player2Name: Stri
                     3 -> score += "Forty"
                 }
             }
-        
+
         return score
     }
 
