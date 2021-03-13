c2c1ef1d22a09fa5d1facc852a1bb48d965f927d
Corentin Leffy <corentin.leffy@gmail.com>
2021-03-13 20:33:11 +0100

    Fixed everything.

--- kotlin/src/main/kotlin/ScoreState.kt
+++ kotlin/src/main/kotlin/ScoreState.kt
@@ -2,4 +2,7 @@ interface ScoreState {
     val tennisGame: TennisGame1
     fun get(): String
     fun next(): ScoreState
+
+    val areScoreEqual: Boolean get() = tennisGame.player1.points == tennisGame.player2.points
+
 }
