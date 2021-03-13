0dfba16bcab17deacffa83979b4a11cff1dc905d
Corentin Leffy <corentin.leffy@gmail.com>
2021-03-13 18:02:55 +0100

    Updated build targets.

--- kotlin/src/main/kotlin/TennisGame1.kt
+++ kotlin/src/main/kotlin/TennisGame1.kt
@@ -1,27 +1,27 @@
 class TennisGame1(private val player1Name: String, private val player2Name: String) : TennisGame {
 
     private var score1: Int = 0
-    private var m_score2: Int = 0
+    private var score2: Int = 0
 
     override fun wonPoint(playerName: String) {
         if (playerName === "player1")
             score1 += 1
         else
-            m_score2 += 1
+            score2 += 1
     }
 
     override fun getScore(): String {
         var score = ""
         var tempScore: Int
-        if (score1 == m_score2) {
+        if (score1 == score2) {
             score = when (score1) {
                 0 -> "Love-All"
                 1 -> "Fifteen-All"
                 2 -> "Thirty-All"
                 else -> "Deuce"
             }
-        } else if (score1 >= 4 || m_score2 >= 4) {
-            val minusResult = score1 - m_score2
+        } else if (score1 >= 4 || score2 >= 4) {
+            val minusResult = score1 - score2
             score = when {
                 minusResult == 1 -> "Advantage player1"
                 minusResult == -1 -> "Advantage player2"
@@ -34,7 +34,7 @@ class TennisGame1(private val player1Name: String, private val player2Name: Stri
                     tempScore = score1
                 else {
                     score += "-"
-                    tempScore = m_score2
+                    tempScore = score2
                 }
                 when (tempScore) {
                     0 -> score += "Love"
