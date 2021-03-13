407fdfe507db5828ffdf10ce73a6f9daafacec9d
Corentin Leffy <corentin.leffy@gmail.com>
2021-03-13 18:12:56 +0100

    <<<<<<<<<<<<<<< HEAD

--- kotlin/src/main/kotlin/TennisGame1.kt
+++ kotlin/src/main/kotlin/TennisGame1.kt
@@ -26,11 +26,11 @@ class TennisGame1(private val player1Name: String, private val player2Name: Stri
                 score += "-"
                 tempScore = score2
             }
-            when (tempScore) {
-                0 -> score += "Love"
-                1 -> score += "Fifteen"
-                2 -> score += "Thirty"
-                3 -> score += "Forty"
+            score += when (tempScore) {
+                0 -> "Love"
+                1 -> "Fifteen"
+                2 -> "Thirty"
+                else -> "Forty"
             }
         }
 
