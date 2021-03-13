3eea96d163862b5de665df18d7945a52666121fd
Corentin Leffy <corentin.leffy@gmail.com>
2021-03-13 20:11:02 +0100

    include shit

--- kotlin/src/main/kotlin/TennisGame1.kt
+++ kotlin/src/main/kotlin/TennisGame1.kt
@@ -17,14 +17,6 @@ class TennisGame1(player1Name: String, player2Name: String) : TennisGame {
 
     internal val scoresAreEqual get() = player1.points == player2.points
 
-    private val equalScore
-        get() = when (player1.points) {
-            0 -> "Love-All"
-            1 -> "Fifteen-All"
-            2 -> "Thirty-All"
-            else -> "Deuce"
-        }
-
     internal val aPlayerHasAdvantage get() = player1.points >= 4 || player2.points >= 4
 
     internal val aPlayerIsLeadingByOnePoint get() = abs(player1.points - player2.points) == 1
@@ -32,13 +24,6 @@ class TennisGame1(player1Name: String, player2Name: String) : TennisGame {
     internal val aPlayerIsLeadingByTwoPoints get() = abs(player1.points - player2.points) >= 2
 
     internal val leadingPlayer get() = player1.takeIf { player1.points > player2.points } ?: player2
-
-    private fun Int.toScore() = when (this) {
-        0 -> "Love"
-        1 -> "Fifteen"
-        2 -> "Thirty"
-        else -> "Forty"
-    }
 
     internal fun changeState(newScoreState: ScoreState) {
         scoreState = newScoreState
